c0c91e5b8ac4cfaaa9d8bc451ae044f9ecaebce5
Iam Fernando <fernandoespiry.99@gmail.com>
2025-03-10 23:13:59 -0400

    Initial Commit

---
+++ Maintenance Mate/Maintenance_MateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maintenance_MateApp.swift
+//  Maintenance Mate
+//
+//  Created by Fernando Cardona on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Maintenance_MateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Maintenance MateTests/Maintenance_MateTests.swift
@@ -0,0 +1,17 @@
+//
+//  Maintenance_MateTests.swift
+//  Maintenance MateTests
+//
+//  Created by Fernando Cardona on 3/10/25.
+//
+
+import Testing
+@testable import Maintenance_Mate
+
+struct Maintenance_MateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
